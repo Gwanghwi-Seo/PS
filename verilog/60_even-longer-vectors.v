95a753c9c7f88763f1d26bf8ba06930ccd000445
Gwanghwi-Seo <duty@desk>
2024-07-07 22:17:00 +0900

    ps

---
+++ verilog/60_even-longer-vectors.v
@@ -0,0 +1,11 @@
+module top_module( 
+    input [99:0] in,
+    output [98:0] out_both,
+    output [99:1] out_any,
+    output [99:0] out_different );
+
+for (integer i = 0; i < 99; i = i+1) begin
+    
+end
+
+endmodule

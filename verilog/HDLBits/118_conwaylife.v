cee9b22cd376a7473cfc65aad97b1d2f9c27390e
Gwanghwi-Seo <duty@desk>
2024-12-31 14:30:58 +0900

    ps

--- verilog/HDLBits/118_conwaylife.v
+++ verilog/HDLBits/118_conwaylife.v
@@ -2,23 +2,60 @@ module top_module(
     input clk,
     input load,
     input [255:0] data,
-    output reg [255:0] q ); 
+    output [255:0] q); 
 
-    integer i;
+    genvar i, j;
 
-    wire [3:0] neighbor [256];
+    generate
+        for (i=0; i<16; i=i+1) begin: cell_row
+            for (j=0; j<16; j=j+1) begin: cell_col
+                conway_cell cell_grid(
+                    .clk(clk),
+                    .i_data(data[i*16+j]),
+                    .i_load(load),
+                    .i_neighbor({
+                        data[(((i-1)+16) % 16) * 16 + (((j-1)+16) % 16)],   // left upper
+                        data[(((i-1)+16) % 16) * 16 + j],                   // upper
+                        data[(((i-1)+16) % 16) * 16 + ((j+1) % 16)],          // right upper
+                        data[i*16 + (((j-1)+16) % 16)],                       // left
+                        data[i*16 + ((j+1) % 16)],                            // right
+                        data[((i+1) % 16) * 16 + ((j-1)+16) % 16],          // left down
+                        data[((i+1) % 16) * 16 + j],                        // down
+                        data[((i+1) % 16) * 16 + ((j+1) % 16)]              // right down
+                    }),
+                    .o_q(q[i*16+j])
+                );
+            end
+        end
+    endgenerate
 
-    for (i=0; i<256; i=i+1) begin
-        assign neighbor[i] = q[i][i] & q[i-1][i-1]
+endmodule
+
+module conway_cell(
+    input clk,
+    input i_data,
+    input i_load,
+    input [7:0] i_neighbor,
+    output reg o_q
+);
+
+    reg [3:0] sum;
+
+    always @* begin
+        sum = i_neighbor[0] + i_neighbor[1] + i_neighbor[2] + 
+              i_neighbor[3] + i_neighbor[4] + i_neighbor[5] + 
+              i_neighbor[6] + i_neighbor[7];
     end
 
     always @(posedge clk) begin
-        if (load)
-            q <= data;
-        else begin
-            for (i=0; i<256; i=i+1) begin
-                
-            end
+        if (i_load) begin
+            o_q <= i_data;
+        end else begin
+            case (sum)
+                4'd2: o_q <= o_q;    // 상태 유지
+                4'd3: o_q <= 1'b1;   // 활성화
+                default: o_q <= 1'b0; // 비활성화
+            endcase
         end
     end
 

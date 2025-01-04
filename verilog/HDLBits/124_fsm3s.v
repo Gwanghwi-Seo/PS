f1e8bc6e5fa0624e126d8675c5d3c4a95c3191ed
Gwanghwi-Seo <gwanghwiwest@gmail.com>
2025-01-04 09:23:59 +0900

    ps

---
+++ verilog/HDLBits/124_fsm3s.v
@@ -0,0 +1,45 @@
+module top_module(
+    input clk,
+    input in,
+    input reset,
+    output out); //
+
+    localparam A = 4'b0001,
+               B = 4'b0010,
+               C = 4'b0100,
+               D = 4'b1000;
+
+    reg [3:0] state, next_state;
+
+    // State transition logic
+    always @* begin
+        case (state)
+            A: begin
+                next_state = in ? B : A;
+            end
+            B: begin
+                next_state = in ? B : C;
+            end
+            C: begin
+                next_state = in ? D : A;
+            end
+            D: begin
+                next_state = in ? B : C;
+            end
+        endcase
+    end
+
+    // State flip-flops with asynchronous reset
+    always @(posedge clk) begin
+        if(reset) begin
+            state <= A;
+        end else begin
+            state <= next_state;
+        end
+    end
+
+    // Output logic
+
+    assign out = (state == D) ? 1'b1 : 1'b0;
+
+endmodule

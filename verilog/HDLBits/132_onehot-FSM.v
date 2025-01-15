e132f17da1012174b3026168463c0f643c46e353
Gwanghwi-Seo <gwanghwiwest@gmail.com>
2025-01-15 14:07:27 +0900

    ps

--- verilog/HDLBits/132_onehot-FSM.v
+++ verilog/HDLBits/132_onehot-FSM.v
@@ -5,72 +5,53 @@ module top_module(
     output out1,
     output out2);
 
-   // localparam S0 = 10'b0000000001,
-   //            S1 = 10'b0000000010,
-   //            S2 = 10'b0000000100,
-   //            S3 = 10'b0000001000,
-   //            S4 = 10'b0000010000,
-   //            S5 = 10'b0000100000,
-   //            S6 = 10'b0001000000,
-   //            S7 = 10'b0010000000,
-   //            S8 = 10'b0100000000,
-   //            S9 = 10'b1000000000;
-
-    // State transition logic
-    always @* begin
-        if ((state[0] & in))
-            next_state[0] = 1'b1;
-        else
-            next_state[0] = 1'b0;
-
-        if ((state[1]) & in)
-            next_state[1] = 1'b1;
-        else
-            next_state[1] = 1'b0;
-
-        if ((state[2]) & in)
-            next_state[2] = 1'b1;
-        else
-            next_state[2] = 1'b0;
-
-        if ((state[3]) & in)
-            next_state[3] = 1'b1;
-        else
-            next_state[3] = 1'b0;
-
-        if ((state[4]) & in)
-            next_state[4] = 1'b1;
-        else
-            next_state[4] = 1'b0;
-
-        if ((state[5]) & in)
-            next_state[5] = 1'b1;
-        else
-            next_state[5] = 1'b0;
-
-        if ((state[6]) & in)
-            next_state[6] = 1'b1;
-        else
-            next_state[6] = 1'b0;
-
-        if ((state[7]) & in)
-            next_state[7] = 1'b1;
-        else
-            next_state[7] = 1'b0;
-
-        if ((state[8]) & in)
-            next_state[8] = 1'b1;
-        else
-            next_state[8] = 1'b0;
-
-        if ((state[9]) & in)
-            next_state[9] = 1'b1;
-        else
-            next_state[9] = 1'b0;
+    always @(*) begin
+        next_state = 0;
+        if (state[0]) begin
+            if (in) next_state[1] = 1;
+            else next_state[0] = 1;
+        end
+        if (state[1]) begin
+            if (in) next_state[2] = 1;
+            else next_state[0] = 1;
+        end
+        if (state[2]) begin
+            if (in) next_state[3] = 1;
+            else next_state[0] = 1;
+        end
+        if (state[3]) begin
+            if (in) next_state[4] = 1;
+            else next_state[0] = 1;
+        end
+        if (state[4]) begin
+            if (in) next_state[5] = 1;
+            else next_state[0] = 1;
+        end
+        if (state[5]) begin
+            if (in) next_state[6] = 1;
+            else next_state[8] = 1;
+        end
+        if (state[6]) begin
+            if (in) next_state[7] = 1;
+            else next_state[9] = 1;
+        end
+        if (state[7]) begin
+            if (in) next_state[7] = 1;
+            else next_state[0] = 1;
+        end
+        if (state[8]) begin
+            if (in) next_state[1] = 1;
+            else next_state[0] = 1;
+        end
+        if (state[9]) begin
+            if (in) next_state[1] = 1;
+            else next_state[0] = 1;
+        end
     end
 
-    // output logic
-    assign out1 = ((state[8]) | (state[9])) ? 1'b1 : 1'b0;
-    assign out2 = ((state[7]) | (state[9])) ? 1'b1 : 1'b0;
+    assign out2 = (state[7] | state[9]) ? 1'b1 : 1'b0;
+    assign out1 = (state[8] | state[9]) ? 1'b1 : 1'b0;
+
+    //정답이 약간 잘못된 듯 함. case로 state transition이 맞을 듯
 
 endmodule
